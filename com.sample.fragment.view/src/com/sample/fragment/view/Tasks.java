3b3fba264b66218e962854d7875a15d0ff06ac80
Joseph Carroll <jdsalingerjr@gmail.com>
2012-07-06 09:26:20 -0500

    e4 fragment example

---
+++ com.sample.fragment.view/src/com/sample/fragment/view/Tasks.java
@@ -0,0 +1,21 @@
+ 
+package com.sample.fragment.view;
+
+import javax.inject.Inject;
+import org.eclipse.e4.ui.di.Focus;
+
+public class Tasks {
+	@Inject
+	public Tasks() {
+		//TODO Your code here
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+}
