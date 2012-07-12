3a9e479d2160ebf5d8854dd2901d3c79ec1ccba0
Joseph Carroll <jdsalingerjr@gmail.com>
2012-07-11 20:57:05 -0500

    PerspectiveSwitcher first run

---
+++ com.sample.application/src/com/sample/application/Welcome.java
@@ -0,0 +1,21 @@
+ 
+package com.sample.application;
+
+import javax.inject.Inject;
+import org.eclipse.e4.ui.di.Focus;
+
+public class Welcome {
+	@Inject
+	public Welcome() {
+		//TODO Your code here
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+}
